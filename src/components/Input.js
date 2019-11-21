13ef9d74213d9e8fa0e26322477503985e165fbb
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-11-21 13:51:46 +0100

    add placeholder

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Input = props => {
-  return <input type="text" value={props.value} onChange={props.change} />;
+  return <input type = "text"
+  placeholder = 'wpisz miasto'
+  value = {
+    props.value
+  }
+  onChange = {
+    props.change
+  }
+  />;
 };
 
 export default Input;
