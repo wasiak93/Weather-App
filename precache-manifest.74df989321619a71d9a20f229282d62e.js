0bbc05cad1297395477d707b233faebba5ff7b31
Kacper <kacperjacekwasiak@gmail.com>
2020-01-09 19:32:51 +0100

    Updates

---
+++ precache-manifest.74df989321619a71d9a20f229282d62e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e2bcae30b73d2fa509d37be5156d6f",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "3f2c8fa89e08b19bc26c",
+    "url": "/Weather-App/static/css/main.14d0d620.chunk.css"
+  },
+  {
+    "revision": "aa9021cc7dfc3df170a9",
+    "url": "/Weather-App/static/js/2.3aba7d0f.chunk.js"
+  },
+  {
+    "revision": "3f2c8fa89e08b19bc26c",
+    "url": "/Weather-App/static/js/main.1afcab7c.chunk.js"
+  },
+  {
+    "revision": "e6c3feae64726e096ecc",
+    "url": "/Weather-App/static/js/runtime-main.35a429e0.js"
+  }
+]);
